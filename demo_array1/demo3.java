39bd2ad80d3c1ebd08a65fdfc668bae82fd8d2b6
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2022-08-31 17:34:43 -0300

    demo array 1

---
+++ demo_array1/demo3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo_array1;
+
+/**
+ *
+ * @author eperez50
+ */
+public class demo3 {
+    
+    public void compara() {
+        
+        mydate [ ] dates= { new mydate(7,7,1968), new mydate(22,8,1976), new mydate(5,7,2004) }; 
+        
+    }
+    
+}
---
+++ demo_array1/mydate.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo_array1;
+
+/**
+ *
+ * @author eperez50
+ */
+
+public class mydate { 
+    private int day; 
+    private int month; 
+    private int year; 
+    
+    public mydate(int day, int month,int year){ 
+        this.day=day; 
+        this.month=month; 
+        this.year=year; 
+    } 
+} 
+
