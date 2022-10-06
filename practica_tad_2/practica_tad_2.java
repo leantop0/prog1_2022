63d02925dda9a5d6d39ce8514438bdc84b4478b0
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2022-10-06 16:42:23 -0300

    Proyecto de ejemplo metodos de los TAD

---
+++ practica_tad_2/practica_tad_2.java
@@ -0,0 +1,19 @@
+package practica_tad_2;
+
+/**
+ *
+ * @author eduardo.perez
+ */
+public class practica_tad_2 {
+
+    public static void main(String[] args) {
+        
+        Test_Stack ts = new Test_Stack();
+        ts.demo_stack();
+        
+        test_queue tq = new test_queue();
+        tq.demo_queue();
+        
+    }
+    
+}
