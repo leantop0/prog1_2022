5fad9e2dfd3006eb644a86bc2ca8911762396d89
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2022-10-06 16:29:58 -0300

    Codigo complero

--- practca_tad_1/menu_tad1.java
+++ practca_tad_1/menu_tad1.java
@@ -1,8 +1,10 @@
 package practca_tad_1;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author 
+ * @author Eduardo
  */
 public class menu_tad1 {
     
@@ -12,7 +14,7 @@ public class menu_tad1 {
         
         // 1 - crear un objeto ArrayList
         // llenarlo con los datos siguientes:
-        // blanoco
+        // blanco
         // negro
         // marron
         // rojo
@@ -23,13 +25,36 @@ public class menu_tad1 {
         // indigo
         // cyan
         
+        ArrayList<String> array1 = new ArrayList();
+        
+        array1.add("blanco");
+        array1.add("negro");
+        array1.add("marron");
+        array1.add("rojo");
+        array1.add("verde");
+        array1.add("azul");
+        array1.add("violeta");
+        array1.add("amarillo");
+        array1.add("indigo");
+        array1.add("cyan");
+        
+        System.out.println("---------------------------------");
+        for(String elemento:array1) {
+            System.out.println(elemento);
+        }
+            
+        
         // 2 - crear un objeto ejercicio_tad1
+        ejercicio_tad1 etad1 = new ejercicio_tad1();
         
+        etad1.llenar_pila(array1);
+        etad1.print_pila();
+
         // 3 - llamar a los metodos de ejercicio_tad1
-        
+
+        etad1.llenar_cola(array1);
+        etad1.print_cola();
         
     }
-            
-    
     
 }
