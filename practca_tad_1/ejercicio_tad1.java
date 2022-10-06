902ac22be0837543e450fc75f933eb5c8cea3fbf
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2022-10-06 16:30:42 -0300

    Codigo completo

--- practca_tad_1/ejercicio_tad1.java
+++ practca_tad_1/ejercicio_tad1.java
@@ -1,8 +1,13 @@
 package practca_tad_1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  *
- * @author 
+ * @author Eduardo
  */
 public class ejercicio_tad1 {
     
@@ -10,19 +15,55 @@ public class ejercicio_tad1 {
 
     // 1 - agregar a esta clase un objeto pila (stack) y un objeto cola (queue)
     
+    Stack<String> pila1 = new Stack();
+    Queue<String> str_queue = new LinkedList<> ();
+    
     // 2 - escribir un metodo llenar_pila, que tiene como entrada el Arraylist de menu_tad1
     // en llenar_pila se debe recorrer el arrayList de entrada y llenar con esos datos el 
     // objeto pila de la clase
     
+    public void llenar_pila(ArrayList<String> parArray) {
+        
+        for(String elemento_array: parArray) {
+            this.pila1.push(elemento_array);
+        }
+
+    }
+    
     // 3 - escribir un metodo print_pila
     // en print_pila se debe recorrer la pila e imprimir cada valor obtenido en consola
     
-    // 4 - escribir un metodo llenar_cola, que tiene como entrada el Arraylist de menu_tad1
+    public void print_pila() {
+
+        System.out.println("---------------------------------");
+        while (!this.pila1.empty()) {
+            String elemento_pila = this.pila1.pop();
+            System.out.println(elemento_pila);
+        }
+    }
+    
+    // 3 - escribir un metodo llenar_cola, que tiene como entrada el Arraylist de menu_tad1
     // en llenar_cola se debe recorrer el arrayList de entrada y llenar con esos datos el 
     // objeto cola de la clase
     
-    // 3 - escribir un metodo print_cola
+    public void llenar_cola(ArrayList<String> parArray) {
+        
+        for(String elemento_array: parArray) {
+            this.str_queue.add(elemento_array);
+        }
+
+    }
+    
+    // 4 - escribir un metodo print_cola
     // en print_cola se debe recorrer la cola e imprimir cada valor obtenido en consola
+
+    public void print_cola() {
+        System.out.println("---------------------------------");
+        while (this.str_queue.size()>0) {
+            String elemento_cola = this.str_queue.remove();
+            System.out.println(elemento_cola);
+        }
+    }
     
 }
 
